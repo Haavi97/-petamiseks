7a3782db82646b88671e2b6f169b67a4688b432f
Haavi <javierortincervera@gmail.com>
2021-10-18 11:33:25 +0300

    Parem menüü. Õpilaste kiire tõlkimiseks.

--- tekst_ascii/tekst_ascii_arvudeks.py
+++ tekst_ascii/tekst_ascii_arvudeks.py
@@ -1,6 +1,7 @@
 def tekst_ascii_arvudeks(tekst: str) -> str:
     return ' '.join(map(lambda x: str(ord(x)), tekst))
 
+
 def ascii_arvudest_tekstiks(tekst: str) -> str:
     return ''.join(map(lambda x: chr(int(x)), tekst.split(' ')))
 
@@ -21,9 +22,34 @@ def ascii_arvudest_tekstiks_failist(fn: str, fout: str) -> str:
     return output
 
 
+def display_menu() -> None:
+    print('Vali mida tegema:')
+    print('\t1. Arvust tekstiks.')
+    print('\t2. Tekstist arvuks.')
+    print('\t3. Arvust tekstiks. Failid')
+    print('\t4. Tekstist arvuks. Failid')
+    print('\t5. Vaikimisi. Näide\n')
+    print('\t0, 1 või exit lõpetamiseks.\n')
+
+
 if __name__ == '__main__':
-    print("Tõlkides antud 'näide.txt' -> 'väljund.txt':")
-    print(tekst_ascii_arvudeks_failist('näide.txt', 'väljund.txt'))
-    print('Kontrollides, et see töötab vastupidi ka. ')
-    print(ascii_arvudest_tekstiks_failist('väljund.txt', 'tagasi.txt'))
-    print(tekst_ascii_arvudeks(input('Palun, sisesta teksti muundamiseks: ')))
+    while True:
+        display_menu()
+        user_input = input()
+        if user_input in ['0', 'q', 'exit']:
+            break
+        elif user_input == '1':
+            print(ascii_arvudest_tekstiks(input('Palun, sisesta sõne: ')))
+        elif user_input == '2':
+            print(tekst_ascii_arvudeks(input('Palun, sisesta sõne: ')))
+        elif user_input == '3':
+            print(ascii_arvudest_tekstiks_failist(
+                input('Palun, sisesta sisend faili: '), 'väljund.txt'))
+        elif user_input == '4':
+            print(tekst_ascii_arvudeks_failist(
+                input('Palun, sisesta sisend faili: '), 'väljund.txt'))
+        elif user_input == '5':
+            print(tekst_ascii_arvudeks_failist(
+                'näide.txt', 'väljund.txt'))
+        else:
+            print('Palun, sisestage õiget valikud.')
