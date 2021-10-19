e3fd8c2c4a337bd0acd28b07907691b7186d49eb
Haavi <javierortincervera@gmail.com>
2021-10-19 11:34:51 +0300

    Kahendsüsteemi suvaline arvu generaator

---
+++ kahendsüsteem/juhuslik_arv.py
@@ -0,0 +1,12 @@
+from random import randint
+from bitstring import BitArray
+
+def juhuslik_arv(maximum) -> str:
+    return BitArray(hex(randint(0,maximum))).bin
+
+if __name__ == "__main__":
+    while True:
+        user_input = input('\nPalun, sisesta maximum täisarvu: ')
+        if user_input == '0':
+            break
+        print(juhuslik_arv(int(user_input)))
