8db5031ffe328e532527d5817282aa9ade8bec80
Haavi97 <fortin@ttu.ee>
2022-01-26 09:19:24 +0200

    Sõnastiku ülesanne

--- telegram_bot/harjutused/harjuta.py
+++ telegram_bot/harjutused/harjuta.py
@@ -10,14 +10,14 @@ harjutused = get_json('harjutused.json', script=__file__)
 
 def harjuta(update, context):
     """Sellega käsitletakse õpilaste skripte"""
-    aktsepteeritud = ['0', '1', '2']
-    if context.args[0] in aktsepteeritud:
+    try:
+        aktsepteeritud = [0, 1, 2]
         update.message.reply_text(choice(harjutused['0' + context.args[0] + '_tase']),
                                   parse_mode='Markdown')
         kasutaja = update['message']['chat']
         logger.info('kasutaja: ' +
                     str(kasutaja['username']) + ' id: ' + str(kasutaja['id']))
-    else:
+    except:
         update.message.reply_text(
             'Õige tase ei olnud valitud. Aktsepteeritud: ' + str(aktsepteeritud))
         logger.info('Ei olnud õige valik. Context args: ' + str(context.args))
